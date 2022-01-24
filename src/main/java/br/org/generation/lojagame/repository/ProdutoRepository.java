226448d0997779c44366674e098a026c4d350072
Natielle Arruda <78966664+NatielleArruda@users.noreply.github.com>
2022-01-24 20:52:52 -0300

    Loja Game

---
+++ src/main/java/br/org/generation/lojagame/repository/ProdutoRepository.java
@@ -0,0 +1,19 @@
+package br.org.generation.lojagame.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.org.generation.lojagame.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+
+	 
+	public List <Produto> findByPrecoGreaterThanOrderByPreco(BigDecimal preco);
+	
+	 
+	public List <Produto> findByPrecoLessThanOrderByPrecoDesc(BigDecimal preco);
+	
+}
